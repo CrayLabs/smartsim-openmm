55ca7d61587c4563d5b62c78602148542c4bfa92
Al Rigazzi <al.rigazzi@hpe.com>
2021-10-08 11:06:05 +0000

    Clean up cobalt submit script

--- cobalt_submit_small.sh
+++ cobalt_submit_small.sh
@@ -3,7 +3,6 @@
 #COBALT -n 4
 #COBALT -q full-node 
 #COBALT -A datascience
-#COBALT --cwd=/home/arigazzi
 
 
 . /lus/theta-fs0/software/thetagpu/conda/2021-06-26/mconda3/etc/profile.d/conda.sh
@@ -13,4 +12,4 @@ export LD_LIBRARY_PATH=/lus/theta-fs0/software/thetagpu/openmpi-4.0.5/lib:/lus/t
 export PATH=/lus/theta-fs0/software/thetagpu/openmpi-4.0.5/bin/:/lus/theta-fs0/software/thetagpu/openmpi-4.0.5/ucx-1.9.0rc7/bin:$PATH
 
 cd /lus/theta-fs0/projects/datascience/arigazzi/smartsim-dev/smartsim-openmm
-pythom smartsim_md_thetagpu.py
+python smartsim_md_thetagpu.py
